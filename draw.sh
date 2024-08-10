51d8ae544a644e2845fb7e96eaa87cfde5b55e8e
albertonoys <albertonoys@gmail.com>
2024-08-10 13:13:10 +0200

    Add readme with preview and layout script

---
+++ draw.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Function to generate SVG for a keymap file
+generate_svg_for_keymap() {
+    local keymap_file="$1"
+    local keymap_name=$(basename "$keymap_file" .keymap)
+    local output_yaml="keymap-drawer/${keymap_name}.yaml"
+    local output_svg="keymap-drawer/${keymap_name}.svg"
+
+    # Parse the keymap file and create SVG
+    keymap --config "keymap-drawer/config.yml" parse -z $keymap_file -o $output_yaml &&
+    keymap --config "keymap-drawer/config.yml" draw $output_yaml -o $output_svg
+
+    echo "SVG generated: $output_svg"
+}
+
+# Find all .keymap files in the config directory and generate SVGs
+find config -name "*.keymap" | while read -r keymap_file; do
+    generate_svg_for_keymap "$keymap_file"
+done
---
+++ readme.md
@@ -0,0 +1,3 @@
+ZMK config for Aurora Sweep
+
+![Aurora Sweep Keymap](keymap-drawer/splitkb_aurora_sweep.svg)
